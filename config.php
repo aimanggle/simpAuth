af797d538a178291f8ca492ad09e249f22286696
aimanggle <aimanggle21@gmail.com>
2022-11-14 11:27:07 +0800

    config file

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
 //connect to database using prodedural method
-$conn = mysqli_connect("localhost", "root", "", "simpAuth");
+$conn = mysqli_connect("localhost", "root", "", "simpauth");
 
 //check connection
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
+}
+else{
+    echo "Connected successfully";
 }
 
 
