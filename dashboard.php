3f9e38b41cf2f88bf5beb512bfbfe30b110669f2
aimanggle <aimanggle21@gmail.com>
2022-11-14 11:45:21 +0800

    add logout button

--- dashboard.php
+++ dashboard.php
@@ -12,6 +12,8 @@ require 'filterAuth.php';
   </head>
   <body>
     <h1>Dashbaord Page</h1>
+
+    <a href="logout.php" class="btn btn-primary">Logout</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
